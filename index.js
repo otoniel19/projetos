9ff2e519118617eee824e35ff962dbc57e6b7fb8
otoniel19 <otonielcostaoliveira3@gmail.com>
2021-06-23 14:36:21 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,50 @@
+//função da Idade
+function idade() {
+let anoAtual = parseInt(prompt("em que ano você está?"))
+
+let anoNasceu = parseInt(prompt("em que ano você nasceu?"))
+
+atual = (anoAtual)
+nasceu = (anoNasceu)
+
+let idadeTotal = (anoAtual - anoNasceu)
+
+window.confirm(` VOCÊ ESTA EM ${atual} ENTÃO VOCÊ TEM ${idadeTotal} ANOS `)
+return;
+  
+}
+
+//função da calculadora
+function calculadora() {
+  alert("digite + para adição - para subtração * para multiplicação / para divisão obs: no operador")
+  let primeiro = parseInt(prompt("digite o primeiro número"))
+  let operador = prompt("digite um operador")
+  let segundo = parseInt(prompt("digite o segundo número"))
+
+a1 = (primeiro)
+a2 = (operador)
+a3 = (segundo)
+  
+  switch (operador) {
+    case "+":
+      let mais = (a1 + a3)
+      
+      window.confirm(` ${a1} + ${a3} e igual a ${mais} `)
+  break;
+     case "-":
+       let menos = (a1 - a3)
+
+       window.confirm(` ${a1} - ${a3} e igual a ${menos} `)
+  break;
+      case "*":
+        let multiplicação = (a1 * a3)
+        
+        window.confirm(` ${a1} * ${a3} e igual a ${multiplicação} `)
+  break;
+      case "/":
+         let divisão = (a1 / a3)
+         
+         window.confirm(` ${a1} / ${a3} e igual a ${divisão} `)
+    
+  }
+}

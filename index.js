97b35237c9b3692c5310635fe2c7f64fd3346f13
otoniel19 <84187511+otoniel19@users.noreply.github.com>
2021-06-23 16:25:26 -0300

    update index.js

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 //função da Idade
 function idade() {
-let anoAtual = parseInt(prompt("em que ano você está?"))
+  let anoAtual = parseInt(prompt("em que ano você está?"))
 
-let anoNasceu = parseInt(prompt("em que ano você nasceu?"))
+  let anoNasceu = parseInt(prompt("em que ano você nasceu?"))
 
-atual = (anoAtual)
-nasceu = (anoNasceu)
+  atual = (anoAtual)
+  nasceu = (anoNasceu)
 
-let idadeTotal = (anoAtual - anoNasceu)
+  let idadeTotal = (anoAtual - anoNasceu)
 
-window.confirm(` VOCÊ ESTA EM ${atual} ENTÃO VOCÊ TEM ${idadeTotal} ANOS `)
-return;
-  
+  window.confirm(` VOCÊ ESTA EM ${atual} ENTÃO VOCÊ TEM ${idadeTotal} ANOS `)
+  return;
 }
 
 //função da calculadora
@@ -21,30 +20,34 @@ function calculadora() {
   let operador = prompt("digite um operador")
   let segundo = parseInt(prompt("digite o segundo número"))
 
-a1 = (primeiro)
-a2 = (operador)
-a3 = (segundo)
-  
+  a1 = (primeiro)
+  a2 = (operador)
+  a3 = (segundo)
+
   switch (operador) {
     case "+":
       let mais = (a1 + a3)
-      
-      window.confirm(` ${a1} + ${a3} e igual a ${mais} `)
-  break;
-     case "-":
-       let menos = (a1 - a3)
 
-       window.confirm(` ${a1} - ${a3} e igual a ${menos} `)
-  break;
-      case "*":
-        let multiplicação = (a1 * a3)
-        
-        window.confirm(` ${a1} * ${a3} e igual a ${multiplicação} `)
-  break;
-      case "/":
-         let divisão = (a1 / a3)
-         
-         window.confirm(` ${a1} / ${a3} e igual a ${divisão} `)
-    
+      window.confirm(` ${a1} + ${a3} e igual a ${mais} `)
+      break;
+    case "-":
+      let menos = (a1 - a3)
+
+      window.confirm(` ${a1} - ${a3} e igual a ${menos} `)
+      break;
+    case "*":
+      let multiplicação = (a1 * a3)
+
+      window.confirm(` ${a1} * ${a3} e igual a ${multiplicação} `)
+      break;
+    case "/":
+      let divisão = (a1 / a3)
+
+      window.confirm(` ${a1} / ${a3} e igual a ${divisão} `)
+
   }
+}
+//função do login
+function login() {
+  alert("ola")
 }
